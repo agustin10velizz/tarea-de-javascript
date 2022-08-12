2ec27339a4b93c5dd6a93cb8438205564652ca90
agustin veliz <agustin10veliz@hotmail.com>
2022-08-12 18:52:08 -0300

    segunda entrega del proyecto final

--- script.js
+++ script.js
@@ -86,23 +86,26 @@ do {
 }
 */
 
-//interectuar html
+//interectuar html 
 
 class Producto{
-    constructor(id,nombre, marca, precio){
+    constructor(id,nombre, marca, precio,img){
         this.id = id
         this.nombre = nombre
         this.marca = marca
         this.precio = precio
+        this.img = img
     }
 }
 
-const productosss1 = new Producto (1,"Mate", "Imperial", "2500")
-const productosss2 = new Producto (2,"Termo", "stanley", "5000")
-const productosss3 = new Producto (3,"Bombilla", "Apacuero", "950")
-const productosss4 = new Producto (4,"Kit de mate", "LaPampa","7000")
+const productosss1 = new Producto (1,"Mate", "Imperial", "$2500","https://d22fxaf9t8d39k.cloudfront.net/7f1bc31e3b67dc2038dec05e2915c9a31e748aff9233e38e7c0ae0b0683dcee1109209.png")  
+const productosss2 = new Producto (2,"Termo", "stanley", "$5000","https://cdn.shopify.com/s/files/1/0238/5366/2285/products/14.2verde_2000x.jpg?v=1605274036")
+const productosss3 = new Producto (3,"Bombilla", "Apacuero", "$950","https://http2.mlstatic.com/D_NQ_NP_898479-MLA45731292464_042021-W.jpg")
+const productosss4 = new Producto (4,"Kit de mate", "LaPampa","$7000","https://http2.mlstatic.com/D_NQ_NP_950260-MLA47443784107_092021-O.webp")
+
 
 const productoss = [productosss1, productosss2, productosss3, productosss4]
+
 
 const mate_productos = document.getElementById("mate_productos")
 
@@ -111,20 +114,22 @@ productoss.forEach( producto =>{
     mate_productos.innerHTML += `
     
  <div class="card" id="producto ${producto.nombre}" style="width: 18rem;">
+ 
     <div class="card-body">
+    
     <h5 class="card-title"> Nombre: ${producto.nombre}</h5>
     <p class="card-text"> Marca: ${producto.marca}</p>
     <p class="card-text"> Precio: ${producto.precio}</p>
-
+    <img src="${producto.img}" class="img" alt="...">
         </div>
     </div>
+
+    
 
     `
 })
 
-
-
-//agregando eventos
+//agregando eventos y haciendo intereaccion con el formulario
 
 class Compras {
     constructor(username, productos, dia_hora) {
@@ -143,6 +148,42 @@ formUser.addEventListener('submit', (event) => {
     let dia_hora = document.getElementById('dia_hora').value
     const user = new Compras(username, productos, dia_hora)
     compra.push(user)
-    console.log(compra)
     formUser.reset()
+    console.table(compra)
+})
+
+
+
+//usando storage y jsnon
+
+localStorage.setItem("bienvenido", "buenas tardes")
+console.log(localStorage.getItem("bienvenido"))
+
+
+//usando el modo oscuro y claro
+const botonNegro = document.getElementById('botonModoOscuro')
+const botonBlanco = document.getElementById('botonModoClaro')
+
+let ModoOscuro
+
+if(localStorage.getItem('ModoOscuro')) {   //si existe el local storage
+    ModoOscuro = localStorage.getItem('ModoOscuro') //consulto el local storage
+} else {
+    localStorage.setItem('ModoOscuro', "ModoClaro") //crearlo
+}
+
+if(ModoOscuro == "dark") {
+    document.body.classList.add("ModoOscuro")
+}
+
+botonNegro.addEventListener('click', () => {
+    
+    document.body.classList.add("ModoOscuro")
+    localStorage.setItem('ModoOscuro', "Modo Oscuro")
+})
+
+botonBlanco.addEventListener('click', () => {
+   
+    document.body.classList.remove("ModoOscuro")
+    localStorage.setItem('ModoOscuro', "Modo Claro")
 })
